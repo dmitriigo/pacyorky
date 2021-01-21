dda3f0bcaf45300f8cc68c36fc89e014fcefc653
Lulkast <Lulkast@yandex.ru>
2021-01-21 15:05:29 +0300

    + telegram logger

---
+++ src/test/java/telegram/TelegramAppenderTest.java
@@ -0,0 +1,18 @@
+package telegram;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TelegramAppenderTest {
+
+    private final Logger logger = LoggerFactory.getLogger(TelegramAppenderTest.class);
+
+    @Test
+    public void test() {
+        logger.info("Test");
+        logger.error("Error");
+    }
+}
